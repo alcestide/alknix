4aad8c86376cbedaf3abe25a844338b85d61a759
Angelo Panariti <angpanariti@gmail.com>
2024-09-23 15:50:46 +0200

    nixos-conf-23-09-2024

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "ALK's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      alknix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.alcestide = import ./home-manager/home.nix;
+
+         }
+        ];
+      };
+    };
+  };
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,95 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      	./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+                "electron-24.8.6"
+              ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "alknix"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Rome";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "it_IT.UTF-8";
+    LC_IDENTIFICATION = "it_IT.UTF-8";
+    LC_MEASUREMENT = "it_IT.UTF-8";
+    LC_MONETARY = "it_IT.UTF-8";
+    LC_NAME = "it_IT.UTF-8";
+    LC_NUMERIC = "it_IT.UTF-8";
+    LC_PAPER = "it_IT.UTF-8";
+    LC_TELEPHONE = "it_IT.UTF-8";
+    LC_TIME = "it_IT.UTF-8";
+  };
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.openssh.enable = true;
+  services.printing.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+};
+
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh ];
+
+  users.users.alcestide = {
+    isNormalUser = true;
+    description = "Angelo Panariti";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+	edk2-uefi-shell
+	ntfs3g
+	htop
+	git
+	mpv
+  	vim
+  ];
+
+  fonts.packages = with pkgs; [
+	  noto-fonts
+	  noto-fonts-cjk
+	  noto-fonts-emoji
+	  liberation_ttf
+	  fira-code
+	  fira-code-symbols
+	  mplus-outline-fonts.githubRelease
+	  dina-font
+	  proggyfonts
+	  nerdfonts
+  ];
+
+  nix.gc = { automatic = true; dates = "weekly"; options = "--delete-older-than 1d"; }; 
+  system.stateVersion = "24.05"; 
+}
