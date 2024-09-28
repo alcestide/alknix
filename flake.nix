d08b3475914ce4343efd817015e1496fbcc719b8
Angelo Panariti <angpanariti@gmail.com>
2024-09-28 16:19:45 +0200

    host-based setup

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     };
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    sops-nix.url = "github:Mic92/sops-nix";
     catppuccin.url = "github:catppuccin/nix";
   };
 
@@ -20,34 +21,45 @@
   } @ inputs: let
     inherit (self) outputs;
     lib = nixpkgs.lib // home-manager.lib;
-    
-    host = "alknix";
+
+    # -------------------
+    # horus: main rig
+    # osiris: home-server
+    # ---------------------------------------
+    # nixos-rebuild- --switch --flake .#{HOST}
+    # ---------------------------------------
+
+    # Change host
+    host = "horus";
+
     username = "alcestide";
-    
     systems = ["x86_64-linux"];
-    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
     pkgsFor = lib.genAttrs systems (system:
       import nixpkgs {
         inherit system;
+        inherit username;
+        inherit host;
       });
   in {
     inherit lib;
-    packages = forEachSystem (pkgs: import ./hosts/${host}/pkgs {inherit pkgs;});
-    formatter = forEachSystem (pkgs: pkgs.nixpkgs-fmt);
-    overlays = import ./hosts/${host}/overlays {inherit inputs;};
+    overlays = import ./hosts/${host}/overlays {inherit inputs host;};
     nixosConfigurations = {
-      ${host} = nixpkgs.lib.nixosSystem {
+      "${host}" = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs username host;};
         modules = [./hosts/${host}/global/configuration.nix 
                     catppuccin.nixosModules.catppuccin
-                   home-manager.nixosModules.home-manager {
-                    home-manager = { 
+                    home-manager.nixosModules.home-manager {
+                home-manager = { 
                     useGlobalPkgs = true;
                     useUserPackages = true;
-                    users.${username} = { imports = [ ./hosts/${host}/home-manager/home.nix
-                                                    #nixvim.homeManagerModules.nixvim
-                                                    catppuccin.homeManagerModules.catppuccin];};
-                  };}];
-                };};
-                };
-              }
+                    users.${username} = 
+                    { imports = 
+                    [ ./hosts/${host}/home-manager/home.nix
+                        #nixvim.homeManagerModules.nixvim
+                        catppuccin.homeManagerModules.catppuccin];};
+                        };
+                }];
+            };
+        };
+        };
+    }
